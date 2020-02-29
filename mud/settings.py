07a7bab5c8481de9c2112b19e293cac0a247f045
ExtraJP2014 <extrajp2014@gmail.com>
2020-02-28 23:44:49 -0500

    added backup server

--- mud/settings.py
+++ mud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['mud-production.herokuapp.com']
+ALLOWED_HOSTS = ['mud-production.herokuapp.com', 'csp5.herokuapp.com']
 
 
 # Application definition
